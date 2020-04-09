cfe80e1c8dcb728c0ea5182b81430f16c346f69e
Michael Padiernos <mike.padiernos@gmail.com>
2020-04-08 23:22:19 -0400

    [FEATURE] API / .get
    - coded .get for route '/:id'

--- routers/accounts-router.js
+++ routers/accounts-router.js
@@ -10,8 +10,34 @@ router
 				res.json(accounts)
 			})
 			.catch(error => {
-				res.status(500).json({success: false, message: "No accounts retrieved", error})
+				res.status(500).json({success: false, message: "Unable to retrieve accounts", error})
 			})
+	// .post((req, res) => {
+	// 	const account = req.body;
+	// 	db('accounts')
+	// 		.insert
+	// })
+	});
+
+router
+	.route('/:id')
+	.get((req, res) => {
+		const id = req.params;
+		db('accounts')
+			// .select('*')
+			// .from('accounts')
+			.where(id)
+			.first()
+			.then(account => {
+				account
+					? res.json({success: true, message: "Account found", account})
+					: res.status(400).json({success: false, message: "Account not fount"})
+			})
+			.catch(error => {
+				res.status(500).json({success: false, message: "Unable to retrieve account", error})
+			})
+	// .put((req, res) => {})
+	// .delete((req, res) => {})
 	});
 
 module.exports = router;
