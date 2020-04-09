50ece411761c2831809a475af0d47ccb43cf15e7
Michael Padiernos <mike.padiernos@gmail.com>
2020-04-08 22:34:45 -0400

    [FEATURE] SQL / middleware
    - added middleware, logger

--- api/server.js
+++ api/server.js
@@ -1,9 +1,10 @@
 const express = require("express");
-
+const middle = require("../middleware/middleware");
 const db = require("../data/dbConfig.js");
 
+const logger = middle.logger;
 const server = express();
 
-server.use(express.json());
+server.use(logger, express.json());
 
 module.exports = server;
